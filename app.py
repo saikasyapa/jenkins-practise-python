3a631d98ebabbb7356faaa46668fe36e7bf4071c
saikasyapa <146060560+saikasyapa@users.noreply.github.com>
2025-09-19 00:35:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Create the Flask app
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World! Flask is running 🚀"
+
+if __name__ == "__main__":
+    # Run the app on localhost:5000
+    app.run(host="0.0.0.0", port=5000, debug=True)
